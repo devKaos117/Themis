cee43aeb7533a99f202b8ed4e52d9524302e9dd3
kaos <gustavo.s.aragao.2003@gmail.com>
2026-02-16 01:03:46 -0300

    minor adjustments

--- kali.sh
+++ kali.sh
@@ -912,8 +912,8 @@ main() {
 	# ====== CLI
 	chsh -s "/bin/zsh" $(id -nu 1000)
 	chsh -s "/bin/zsh" $(id -nu 0)
-	wget "https://github.com/devKaos117/Themis/blob/main/profiles/zshrc" -o "$(getent passwd 1000 | cut -d : -f 6)/.zshrc"
-	wget "https://github.com/devKaos117/Themis/blob/main/profiles/zshrc" -o "$(getent passwd 0 | cut -d : -f 6)/.zshrc"
+	wget "https://raw.githubusercontent.com/devKaos117/Themis/refs/heads/main/profiles/zshrc" -O tmp && mv tmp "$(getent passwd 1000 | cut -d : -f 6)/.zshrc"
+	wget "https://raw.githubusercontent.com/devKaos117/Themis/refs/heads/main/profiles/zshrc" -O tmp && mv tmp "$(getent passwd 0 | cut -d : -f 6)/.zshrc"
 
 	apt install -y zsh-autosuggestions zsh-syntax-highlighting
 	if apt install -y btop ; then
@@ -921,8 +921,8 @@ main() {
 		if [[ ! -d "$(getent passwd 1000 | cut -d : -f 6)/.config/btop" ]]; then mkdir "$(getent passwd 1000 | cut -d : -f 6)/.config/btop"; fi
 		if [[ ! -d "$(getent passwd 0 | cut -d : -f 6)/.config" ]]; then mkdir "$(getent passwd 0 | cut -d : -f 6)/.config"; fi
 		if [[ ! -d "$(getent passwd 0 | cut -d : -f 6)/.config/btop" ]]; then mkdir "$(getent passwd 0 | cut -d : -f 6)/.config/btop"; fi
-		wget "https://github.com/devKaos117/Themis/blob/main/profiles/btop.conf" -o "$(getent passwd 1000 | cut -d : -f 6)/.config/btop/btop.conf"
-		wget "https://github.com/devKaos117/Themis/blob/main/profiles/btop.conf" -o "$(getent passwd 0 | cut -d : -f 6)/.config/btop/btop.conf"
+		wget "https://raw.githubusercontent.com/devKaos117/Themis/refs/heads/main/profiles/btop.conf" -O tmp && mv tmp "$(getent passwd 1000 | cut -d : -f 6)/.config/btop/btop.conf"
+		wget "https://raw.githubusercontent.com/devKaos117/Themis/refs/heads/main/profiles/btop.conf" -O tmp && mv tmp "$(getent passwd 0 | cut -d : -f 6)/.config/btop/btop.conf"
 	fi
 	apt install -y neovim  bat cpufetch fastfetch 7zip mc trash-cli ascii # tldr rclone
 	# ====== VIRT
@@ -939,7 +939,7 @@ main() {
 	# configure snmp
 	# ====== UI
 	# load panel
-	xfce4-panel-profiles load <(curl -s "https://github.com/devKaos117/Themis/blob/main/profiles/Kaos_KaliPanel.tar")
+	wget "https://raw.githubusercontent.com/devKaos117/Themis/refs/heads/main/profiles/Kaos_KaliPanel.tar.bz2" -O tmp && xfce4-panel-profiles load tmp && rm tmp
 	# alter workspace count
 	xfconf-query -c xfwm4 -p /general/workspace_count -n -t int -s 2
 	# disable screensaver
